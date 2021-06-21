f315c0496287a579c508904dc530e3cd39784883
Omkar <73767886+legendomi231@users.noreply.github.com>
2021-06-21 23:46:43 +0530

    Add files via upload

---
+++ weather analyzer/weather.py
@@ -0,0 +1,60 @@
+import requests
+from datetime import datetime
+
+
+api_key = 'efd1050ba511b5071890a39147d4d653'
+location = input("Enter the city name: ")
+
+complete_apk_link = "https://api.openweathermap.org/data/2.5/weather?q=" + location + "&appid=" + api_key
+api_link = requests.get(complete_apk_link)
+api_data = api_link.json()
+
+country = api_data['sys']['country']
+lon = api_data['coord']['lon']
+lat = api_data['coord']['lat']
+temp = ((api_data['main']['temp']) - 273.15)  # - 273.15 used to convert F to C
+weather = api_data['weather'][0]['description']
+humid = api_data['main']['humidity']
+wind_speed = api_data['wind']['speed']
+date_time = datetime.now().strftime("%d %b %Y | %I:%M:%S %p")
+
+
+def weather_write_data():   # Function Declaration
+
+    print("-----------------------------------------------------------------")
+    print("Weather Status of - {} ({}) || {}".format(location.upper(), country.upper(), date_time))
+    print("-----------------------------------------------------------------")
+
+    print(f"Longitude : {lon} || Latitude : {lat}")
+    print("Current temperature is : {:.1f} Deg C".format(temp))
+    print(f"Current weather Description : {weather}")
+    print(f"Current Humidity : {humid} %")
+    print(f"Current wind speed : {wind_speed} KmPh")
+
+    print("-----------------------------------------------------------------")
+    print(f"Complete Link : {complete_apk_link}")
+    print("-----------------------------------------------------------------")
+
+
+weather_write_data()    # Function Call
+
+# -----------------------File Operations Starts Here ------------------------------
+
+file = open(r"weather.txt", "a+")   # append data at the end of file.
+
+file.write("-----------------------------------------------------------------\n")
+file.write("Weather Status of - {} ({}) || {} \n".format(location.upper(), country.upper(), date_time))
+file.write("-----------------------------------------------------------------\n")
+
+file.write(f"Longitude : {lon} || Latitude : {lat} \n")
+file.write("Current temperature is : {:.1f} Deg C\n".format(temp))
+file.write(f"Current weather Description : {weather} \n")
+file.write(f"Current Humidity : {humid} % \n")
+file.write(f"Current wind speed : {wind_speed} kmph\n")
+
+file.write("-----------------------------------------------------------------\n")
+file.write(f"Complete Link : {complete_apk_link}\n")
+file.write("-----------------------------------------------------------------\n")
+file.write("\n")
+
+file.close()
